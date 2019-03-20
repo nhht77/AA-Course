5997b24c6db2f1b65114825ae0c359083551e199
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-03-20 21:33:01 +0200

    SayHi Excercise

---
+++ SayHi/app/src/main/java/trung/nguyen/greetme/MainActivity.java
@@ -0,0 +1,55 @@
+package trung.nguyen.greetme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView = null;
+    TextView textEdit = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textEdit = findViewById(R.id.editText);
+
+        textView.setText("What is your name?");
+
+        findViewById(R.id.btnEng).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = textEdit.getText().toString();
+                textView.setText("Hello " + name + " from London");
+            }
+        });
+
+        findViewById(R.id.btnSuomi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = textEdit.getText().toString();
+                textView.setText("Moi " + name + " Oulusta");
+            }
+        });
+
+        findViewById(R.id.btnJp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = textEdit.getText().toString();
+                textView.setText("東京からこんにちは " + name);
+            }
+        });
+
+        findViewById(R.id.btnCn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = textEdit.getText().toString();
+                textView.setText("西安你好 " + name);
+            }
+        });
+    }
+}
