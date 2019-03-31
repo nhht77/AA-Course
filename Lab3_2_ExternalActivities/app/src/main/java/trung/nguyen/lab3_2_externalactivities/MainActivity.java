b43b4497de5aab6f6fb8e74d45e7dbcc5a392099
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-03-31 18:24:15 +0300

    Lab3_2: External Activities

--- Lab3_2_ExternalActivities/app/src/main/java/trung/nguyen/lab3_2_externalactivities/MainActivity.java
+++ Lab3_2_ExternalActivities/app/src/main/java/trung/nguyen/lab3_2_externalactivities/MainActivity.java
@@ -1,13 +1,60 @@
 package trung.nguyen.lab3_2_externalactivities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn, btn2, btn3;
+    EditText textEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+        textEdit = (EditText) findViewById(R.id.editText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("OPENMAP", "onClick: OPEN MAP");
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                // Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startActivity(mapIntent);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("OPENCALL", "onClick: MAKE A CALL");
+                Uri number = Uri.parse("tel:5551234");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                startActivity(callIntent);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = textEdit.getText().toString();
+                Log.d("text", "onClick: Go to " + text);
+                Uri webpage = Uri.parse(text);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
     }
 }
