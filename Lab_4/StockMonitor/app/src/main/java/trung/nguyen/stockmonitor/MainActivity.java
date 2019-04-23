fbace1582eb52661b07182b0043f616d783dd4e6
nhht77 <35263094+nhht77@users.noreply.github.com>
2019-04-23 08:27:01 +0300

    Stock Monitor: In progress

--- Lab_4/StockMonitor/app/src/main/java/trung/nguyen/stockmonitor/MainActivity.java
+++ Lab_4/StockMonitor/app/src/main/java/trung/nguyen/stockmonitor/MainActivity.java
@@ -1,5 +1,6 @@
 package trung.nguyen.stockmonitor;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         Stock[] stocks  = new Stock[4];
         stocks[0]       = new Stock("AAPL", "Apple", 0);
         stocks[1]       = new Stock("GOOGL", "Google", 0);
@@ -38,7 +42,20 @@ public class MainActivity extends AppCompatActivity {
 
         for (Stock s : stocks) {
             String url = "https://financialmodelingprep.com/api/company/price/" + s.getId() + "?datatype=json";
-            
+            Request request = new Request.Builder().url(url).build();
+            OkHttpClient client = new OkHttpClient();
+
+            try {
+                Response response = client.newCall(request).execute();
+                String body       =  response.body().toString();
+                Gson gson         = new GsonBuilder().create();
+                Log.d("GETSTOCKBODY", "getStock: " + body.toString());
+//                StockId json        =  gson.fromJson(body, StockId.class);
+//                gson.fromJson()
+//                Log.d("GETSTOCKId", "getStock: " +json.getName().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
